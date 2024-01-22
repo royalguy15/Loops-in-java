9b24e7c19e42ec75cef915405bf826d332eef6d2
Rakesh Kumar Nayak <128237968+royalguy15@users.noreply.github.com>
2024-01-22 21:14:00 +0530

    Add files via upload

---
+++ Squarepattern.java
@@ -0,0 +1,19 @@
+package Loops;
+
+public class Squarepattern {
+    public static void main(String args[]) {
+
+        /*
+         * for (int line = 1; line <= 4; line++) {
+         * System.out.println("* * * *");
+         * 
+         * }
+         */
+        int line = 1;
+        while (line <= 4) {
+            System.out.println("* * * *");
+            line++;
+        }
+    }
+
+}
---
+++ naturalnumbersum.java
@@ -0,0 +1,22 @@
+package Loops;
+
+import java.util.*;
+
+public class naturalnumbersum {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("sum is:" + sum);
+    }
+
+}
